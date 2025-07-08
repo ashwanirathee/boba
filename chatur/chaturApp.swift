acdc4dcc1021f68e3ae3c823ba4c8e9db3315eb0
Ashwani Rathee <ashwanirathee.work@gmail.com>
2025-07-07 18:47:15 -0700

    Initial Commit

---
+++ chatur/chaturApp.swift
@@ -0,0 +1,17 @@
+//
+//  chaturApp.swift
+//  chatur
+//
+//  Created by ashwani on 07/07/25.
+//
+
+import SwiftUI
+
+@main
+struct chaturApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chaturTests/chaturTests.swift
@@ -0,0 +1,17 @@
+//
+//  chaturTests.swift
+//  chaturTests
+//
+//  Created by ashwani on 07/07/25.
+//
+
+import Testing
+@testable import chatur
+
+struct chaturTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
